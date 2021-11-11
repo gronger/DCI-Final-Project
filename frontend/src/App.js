d84f12ef9be1f72be74483696651b8112475d388
Joseph Grainger <grrainger7@gmail.com>
2021-11-11 15:39:56 +0100

    frontend basics added

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          konnichiwa welc0me 2 our app lolz 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
