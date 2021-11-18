af5c3c770d02171fc06673b7aaed4cd62f4b69c8
Joseph Grainger <grrainger7@gmail.com>
2021-11-18 10:19:55 +0100

    changes made

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Login from './Login.jsx';
+
+export default function Main() {
+
+
+
+return (
+    <div className="main">
+<h1>Welcome to NameOfApp</h1>
+    </div>
+
+)
+
+
+}
