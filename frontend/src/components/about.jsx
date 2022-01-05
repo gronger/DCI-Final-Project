9266b096c7ae12f4776d58608cbc78604a161010
Joseph Grainger <grrainger7@gmail.com>
2022-01-05 16:01:13 +0100

    text on about page more legible

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -6,8 +6,14 @@ const About = () => {
     <div className="about">
       <h1 className="Evrima">EVRIMA</h1>
       <br />
-      <h2>Welcome to Evrima! The site that makes it easy to find like-minded creative people to connect with and start working on projects together.We came up with Evrima after noticing that many creative people struggle to start finding work. As many creative industries are infamously difficult to enter, we created Evrima so that our users can link up and start creating a portfolio.Our site is for anyone and everyone looking to work on collaborative creative projects. Maybe you just graduated university and are looking to find your first job, maybe you're looking to expand on your portfolio or gain more experience, or perhaps you are a hobby artist, just looking to practise your interests. Everyone is welcome!Our site provides a platform for both sides, Talent (actors, models) and Project Organizers (photographers, film directors, fashion brands) to collaborate.We encourage anyone interested in working in creative industries to join our site, and find out for themselves what they can unleash!
+      <h2 className="Evrima">Welcome to Evrima! The site that makes it easy to find like-minded creative people to connect with and start working on projects together.
 </h2>
+<h2 className="Evrima">
+We came up with Evrima after noticing that many creative people struggle to start finding work. As many creative industries are infamously difficult to enter, we created Evrima so that our users can link up and start creating a portfolio.
+</h2>
+<h2 className="Evrima">Our site is for anyone and everyone looking to work on collaborative creative projects. Maybe you just graduated university and are looking to find your first job, maybe you're looking to expand on your portfolio or gain more experience, or perhaps you are a hobby artist, just looking to practise your interests. Everyone is welcome!</h2>
+<h2 className="Evrima">Our site provides a platform for both sides, Talent (actors, models) and Project Organizers (photographers, film directors, fashion brands) to collaborate.</h2>
+<h2 className="Evrima"> We encourage anyone interested in working in creative industries to join our site, and find out for themselves what they can unleash!</h2>
     </div>
   )
 }
